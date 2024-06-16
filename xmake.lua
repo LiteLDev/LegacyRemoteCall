3276f38c7b2977b3a237d32faa9bf5aef7c4eaf5
ShrBox <shrbox@outlook.com>
2024-06-16 20:32:47 +0800

    chore: update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release", "mode.releasedbg")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-add_requires("levilamina 0.12.0")
+add_requires("levilamina develop")
 
 set_runtimes("MD")
 
