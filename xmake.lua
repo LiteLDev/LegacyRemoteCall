bd9befb83caa2d91af0e291e785b5b12258b8c0a
ShrBox <shrbox@outlook.com>
2025-01-09 15:47:45 +0800

    chore: switch LeviLamina to 1.0.0-rc.2

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 if is_config("target_type", "server") then
     add_requires("levilamina 1.0.0-rc.1", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina 1.0.0-rc.1", {configs = {target_type = "client"}})
+    add_requires("levilamina 1.0.0-rc.2", {configs = {target_type = "client"}})
 end
 
 add_requires("levibuildscript")
