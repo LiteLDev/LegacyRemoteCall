0c9d2595276013ba36a3ef721bd97c3810c6be1c
ShrBox <shrbox@outlook.com>
2026-06-28 14:07:32 +0800

    chore(deps): bump LeviLamina to 26.20

--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,9 @@ add_rules("mode.debug", "mode.release")
 add_repositories("levimc-repo " .. (get_config("levimc_repo") or "https://github.com/LiteLDev/xmake-repo.git"))
 
 if is_config("target_type", "server") then
-    add_requires("levilamina 26.10.*", {configs = {target_type = "server"}})
+    add_requires("levilamina dab5005c569ae9984ed62f20d209e02f77da9f89", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina 26.10.*", {configs = {target_type = "client"}})
+    add_requires("levilamina dab5005c569ae9984ed62f20d209e02f77da9f89", {configs = {target_type = "client"}})
 end
 
 add_requires("levibuildscript")
@@ -13,6 +13,8 @@ add_requires("levibuildscript")
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
+
+set_toolchains("clang-cl")
 
 option("levimc_repo")
     set_default("https://github.com/LiteLDev/xmake-repo.git")
