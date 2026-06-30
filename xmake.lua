02bdeecf8144b434ba158cd22c600ba24a9d9713
ShrBox <shrbox@outlook.com>
2026-06-30 19:21:20 +0800

    chore(xmake): add clang-cl toolchain flags on Windows

--- xmake.lua
+++ xmake.lua
@@ -14,8 +14,6 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-set_toolchains("clang-cl")
-
 option("levimc_repo")
     set_default("https://github.com/LiteLDev/xmake-repo.git")
     set_showmenu(true)
@@ -31,10 +29,26 @@ option_end()
 target("LegacyRemoteCall")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-    add_defines("NOMINMAX", "UNICODE", "REMOTE_CALL_EXPORT")
+    if is_plat("windows") then
+        add_defines("NOMINMAX", "UNICODE", "REMOTE_CALL_EXPORT")
+        set_exceptions("none") -- To avoid conflicts with /EHa.
+        add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+        add_cxflags(
+            "/EHs",
+            "-Wno-microsoft-cast",
+            "-Wno-invalid-offsetof",
+            "-Wno-c++2b-extensions",
+            "-Wno-microsoft-include",
+            "-Wno-overloaded-virtual",
+            "-Wno-ignored-qualifiers",
+            "-Wno-missing-field-initializers",
+            "-Wno-potentially-evaluated-expression",
+            "-Wno-pragma-system-header-outside-header",
+            {tools = {"clang_cl"}}
+        )
+        set_toolchains("clang-cl")
+    end
     add_packages("levilamina")
-    set_exceptions("none")
     set_kind("shared")
     set_languages("c++20")
     set_symbols("debug")
